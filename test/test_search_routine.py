0c2fe58c2c86334b2586f70a362dc16848905bad
Jason Goodman <jcgoodman@gmail.com>
2023-07-08 08:03:49 -0400

    comments

--- test/test_search_routine.py
+++ test/test_search_routine.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-cwd = Path(__file__).parent.resolve() 
+cwd = Path(__file__).parent.resolve() # The directory this file is in
 
 # Modify these as needed
 eso_login="goodmanj"
@@ -8,9 +8,11 @@ starlist= cwd/ "OSETI_targets_subset.txt"
 downloadlist = cwd / "multistarwidefield.txt"
 results=cwd / "OSETI_results.txt"
 
-optical_seti_dir = cwd.parent.resolve() 
-sys.path.append(str(optical_seti_dir))
+optical_seti_dir = cwd.parent.resolve()  # Location of optical SETI scripts
+sys.path.append(str(optical_seti_dir))   # Add to path
 
 import seti_catalog_functions
+# Download
 seti_catalog_functions.predownloader(withlogin=eso_login,withstarlist=starlist,withresults=downloadlist)
+# Analyze
 seti_catalog_functions.do_search(withlogin=eso_login,withstarlist=downloadlist,withresults=results,predownloader_format=True)
